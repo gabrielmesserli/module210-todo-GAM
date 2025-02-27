ac3dd59bb262faea6935342c1e40a24df7047ac8
gabrielmesserli <144122324+gabrielmesserli@users.noreply.github.com>
2025-02-27 08:51:41 +0100

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
+const apiEndpoint = "https://container-chy-backend-messerli.greentree-289154a2.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
